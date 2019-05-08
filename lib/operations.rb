a4db41ef2f1966916f799cd576169403944afd73
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-08 18:07:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,13 @@
-def unsafe?(speed)
-
+def unsafe? = 30
+if speed < 40 "safe"
+else speed < 50 "safe"
+  elsif speed < 60 "safe"
 end
 
 
 
-def not_safe?(speed)
+def not_safe? = speed.below.40 
+  puts "unsafe" if speed_below == 40
 	
 end
 	
